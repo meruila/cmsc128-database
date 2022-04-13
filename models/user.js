16a1278288ad3f02131aa09cee1cace10f1f00f9
Jeano Duhaylungsod <jiduhaylungsod@up.edu.ph>
2022-04-13 13:20:24 +0800

    User Password Bcrypt

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const UserSchema = new mongoose.Schema({
   name:  {
@@ -8,5 +9,26 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true }
 });
+
+UserSchema.pre("save", function(next) {
+  const user = this;
+
+  if (!user.isModified("password")) return next();
+
+  return bcrypt.genSalt((saltError, salt) => {
+    if (saltError) { return next(saltError); }
+
+    return bcrypt.hash(user.password, salt, (hashError, hash) => {
+      if (hashError) { return next(hashError); }
+
+      user.password = hash;
+      return next();
+    });
+  });
+});
+
+UserSchema.methods.comparePassword = function(password, callback) {
+  bcrypt.compare(password, this.password, callback);
+}
 
 module.exports = mongoose.model("User", UserSchema);
