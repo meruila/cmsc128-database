48b316cfab39af427d0c4ca9665896d2255f27ea
LonelyFennec <l14schiffer@gmail.com>
2022-04-25 23:13:53 +0800

    added student-record model (schema)

---
+++ models/student-record.js
@@ -0,0 +1,37 @@
+/*******************************
+ * 
+ * STUDENT RECORD SCHEMA
+ * Attributes:
+ *      Name (first, last)
+ *      Degree Program (reference to Degree model)
+ *      Student Number (Unique Identifier)
+ * 
+********************************/
+
+const mongoose = require("mongoose");       
+
+//schema
+const SR_Schema = new mongoose.Schema({
+    name: {
+        fname: {
+            type: String,
+            required: true
+        },
+        lname: {
+            type: String,
+            required: true
+        }
+    },
+    degree: {
+        type: mongoose.ObjectId,
+        required: true,
+        ref: 'Degree'
+    },
+    studentNumber : {
+        type: Number,
+        unique: true,
+        required: true
+    }
+});
+
+module.exports = mongoose.model("Student-Record", DegreeSchema);
