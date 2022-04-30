f66619457da8f18f1ccc855d2a6e5d0f6ef809cd
Mirella Arguelles <marguelles@up.edu.ph>
2022-04-30 20:50:10 +0800

    reduced subject schema to subject code and units

--- models/subject.js
+++ models/subject.js
@@ -1,17 +1,19 @@
 /**
+ * 
  * subject.js is where the Subject schema is defined.
- * Every subject has a course code, a grade that the student attained, units of the subject,
- * grade points i.e., units * grade, and the summation of the gradepoints of the student.
+ * Every subject has a course code and a specified number of units for the subject.
+ * Credited subjects have a non-zero value for the units while uncredited subjects
+ * (e.g. HK and NSTP) have zero units.
+ * The Subject schema will be used on a Student Record's taken-subject and on the Degree Program
+ * of a Curriculum.
+ * 
  */
 
  const mongoose = require('mongoose');
 
  const SubjectSchema = new mongoose.Schema({
    code: { type: String, unique: true, required: true },
-   grade: { type: String, required: true },
-   units: { type: String, required: true },
-   gradePoints: { type: Number, required: true },
-   gradePointsSummation: { type: Number, required: true },
+   units: { type: Number, required: true }
  });
  
  module.exports = mongoose.model('Subject', SubjectSchema);
