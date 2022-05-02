83d916266d59f02fa110b89e1f0fe8c2668bddf9
LonelyFennec <l14schiffer@gmail.com>
2022-05-03 02:25:12 +0800

    update student record schema: add attributes

--- models/student-record.js
+++ models/student-record.js
@@ -2,16 +2,79 @@
  * 
  * STUDENT RECORD SCHEMA
  * Attributes:
- *      Name (first, last)
- *      Degree Program (reference to Degree model)
- *      Student Number (Unique Identifier)
+ *      Name
+ *          - first                 (String)
+ *          - last                  (String)
+ *      Student Number              (String)
+ *      Degree Program              (Reference to Degree model)
+ *      Total Units Taken           (Number)
+ *      GWA                         (Number)
+ *      Records                     (Array)
+ *          - Semester              (Number)
+ *          - Academic Year         (String)
+ *          - Subjects Taken        (Array)
+ *              - Subject Code      (Reference to Subject model)
+ *              - Grade             (String)
+ *              - Gradepoint        (Number)
+ *              - Summation         (Number)
+ *          - Units                 (Number)
+ *          - Status                (String)
+ *      Verified By                 (Array) (Reference to Admin or User model)
  * 
 ********************************/
 
 const mongoose = require("mongoose");       
 
-//schema
-const SR_Schema = new mongoose.Schema({
+//#region subdocument schema
+//taken subject schema
+const TakenSubject_Schema = new mongoose.Schema({
+    subjectCode: {
+        type: mongoose.ObjectId,
+        required: true,
+        ref: 'Subject'
+    },
+    grade : {
+        type: String,
+        required: true
+    },
+    gradepoint : {
+        type: Number,
+        required: true
+    },
+    summation : {
+        type: Number,
+        required: true
+    }
+});
+
+//semester schema
+const Semester_Schema = new mongoose.Schema({
+    sem: {
+        type: Number,
+        required: true
+    },
+    acadYear: {
+        type: String,
+        required: true
+    },
+    subjects: {
+        type: [TakenSubject_Schema],
+        required: true
+    },
+    units: {
+        type: Number,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true
+    }
+});
+//#endregion
+
+//student record schema
+const StudentRecord_Schema = new mongoose.Schema({
+    //#region single-valued attribute
     name: {
         fname: {
             type: String,
@@ -22,16 +85,44 @@ const SR_Schema = new mongoose.Schema({
             required: true
         }
     },
-    degree: {
+    studentNumber : {
+        type: String,                               //string ba talaga, hindi number??? bale pwede kasi na wala na yung dash
+        unique: true,
+        required: true
+    },
+    degreeProgram: {
         type: mongoose.ObjectId,
         required: true,
         ref: 'Degree'
     },
-    studentNumber : {
+    totalUnitsTaken: {
         type: Number,
-        unique: true,
         required: true
+    },
+    GWA: {
+        type: Number,
+        required: true
+    },
+    //#endregion
+
+    //#region multi-valued attribute
+    records: {
+        type: [Semester_Schema],
+        required: true
+    },
+    verifiedBy: [{
+        type: mongoose.ObjectId,
+        //reference to admin or user model/collection
+        refPath: 'modelReferences' 
+    }],
+
+    //to reference two possible models
+    modelReferences: {
+        type: String,
+        required: true,
+        enum: ['Admin', 'User']
     }
+    //#endregion
 });
 
-module.exports = mongoose.model("Student-Record", SR_Schema);
+module.exports = mongoose.model("Student-Record", StudentRecord_Schema);
