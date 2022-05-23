4ba43887f357e764f6eb52832fdcd0b41cb98e59
Mirella Arguelles <marguelles@up.edu.ph>
2022-05-24 02:16:32 +0800

    curriculum: new BSBIO

--- insertTest.js
+++ insertTest.js
@@ -46,95 +46,91 @@ var ObjectId = require('mongodb').ObjectId;
 // explicitly stated in the curricula.
 
 const degree = {
-	'name': 'BSAPHY',
+	'name': 'BSBIO',
 	'major': 'Thesis',
 	'subjects': [
-		[ObjectId("62728f198c3653b70791d9c9")],
-		// PHYS 101
 		[ObjectId("62728cc3591bd41f02539493")],
 		// CHEM 18
-		[ObjectId("62728cc3591bd41f02539494")],
-		// CHEM 18.1
-		[ObjectId("62728cc3591bd41f02539496")],
-		// MATH 27
-		[ObjectId("626d42befee24c0a7a8e930d")],
-		// ARTS 1
+		[ObjectId("62729007ab87167c1fc0cb81")],
+		//MATH 25
+		[ObjectId("62728cc3591bd41f02539495")],
+		//MCB 11
 		[ObjectId("626d42befee24c0a7a8e9310"),
 		// KAS 1
 		ObjectId("626d42befee24c0a7a8e9311")],
 		// HIST 1
+		[ObjectId("626d42befee24c0a7a8e930d")],
+		// ARTS 1
+		[ObjectId("626d42befee24c0a7a8e9312")],
+		// STS 1
 		[ObjectId("626d44d7eb3a38230c7a3e76")],
 		// HK 11
-		[ObjectId("62728f198c3653b70791d9ca")],
-		// PHYS 102
-		[ObjectId("62728f198c3653b70791d9cb")],
-		// PHYS 111
-		[ObjectId("62728d04ecce07f44e4d1759")],
-		// MATH 28
-		[ObjectId("62728606f5ec17dac940459d")],
-		// COMA 150
+		[ObjectId("62728df25f73ec537aa62c22")],
+		//BIO 11.1
+		[ObjectId("62728cc3591bd41f02539494")],
+		// CHEM 18.1
+		[ObjectId("62728d04ecce07f44e4d175a")],
+		//BIO 30
+		[ObjectId("626d42befee24c0a7a8e9313")],
+		// PI 10
 		[ObjectId("626d44d7eb3a38230c7a3e7a"),
 		// HK 12
 		ObjectId("626d44d7eb3a38230c7a3e7b")],
 		// HK 13
-		[ObjectId("62728f25faca18b473a45e70")],
-		// APHY 10.1
-		[ObjectId("62728f25faca18b473a45e71")],
-		// APHY 101
-		[ObjectId("62728f25faca18b473a45e72")],
-		// PHYS 103
-		[ObjectId("62728f25faca18b473a45e73")],
-		// PHYS 112
-		[ObjectId("626d42befee24c0a7a8e930f")],
-		// ETHICS 1
+		[ObjectId("62729007ab87167c1fc0cb82")],
+		//CHEM 40
+		[ObjectId("62729007ab87167c1fc0cb83")],
+		//CHEM 40.1
+		[ObjectId("62729007ab87167c1fc0cb84")],
+		//BIO 14
+		[ObjectId("62729007ab87167c1fc0cb85")],
+		//BOT 14
+		[ObjectId("62729007ab87167c1fc0cb86")],
+		//ZOO 14
+		[ObjectId("62729007ab87167c1fc0cb87")],
+		//CHEM 160
+		[ObjectId("62729007ab87167c1fc0cb88")],
+		//BIO 150
 		[ObjectId("626d44d7eb3a38230c7a3e81")],
 		// NSTP 1
-		[ObjectId("62728f25faca18b473a45e74")],
-		// PHYS 104
-		[ObjectId("62728f25faca18b473a45e75")],
-		// PHYS 113
-		[ObjectId("62728f25faca18b473a45e76")],
-		// PHYS 121
-		[ObjectId("628124c3daea5e7e71bb522b")],
-		// PHYS 131
+		[ObjectId("62728e10280e1499e84e2233")],
+		//PHYS 51
+		[ObjectId("62728e10280e1499e84e2234")],
+		//PHYS 51.1
+		[ObjectId("627290194e19b2ffd466b9cd")],
+		//BIO 101
+		[ObjectId("627290194e19b2ffd466b9ce")],
+		//CHEM 160.1
+		[ObjectId("627290194e19b2ffd466b9cf")],
+		//BIO 140
+		[ObjectId("627290194e19b2ffd466b9d0")],
+		//ABME 10
 		[ObjectId("626d44d7eb3a38230c7a3e85")],
 		// NSTP 2
-		[ObjectId("62728f25faca18b473a45e77")],
-		// APHY 102
-		[ObjectId("62728f25faca18b473a45e78")],
-		// PHYS 115
-		[ObjectId("62728f25faca18b473a45e79")],
-		// PHYS 122
-		[ObjectId("62728f25faca18b473a45e7a")],
-		// PHYS 132
-		[ObjectId("62728f25faca18b473a45e7b")],
-		// PHYS 192.1
-		[ObjectId("626d42befee24c0a7a8e9313")],
-		// PI 10
-		[ObjectId("62728f25faca18b473a45e7c")],
-		// PHYS 141
-		[ObjectId("62728f25faca18b473a45e7d")],
-		// PHYS 151
-		[ObjectId("62728f25faca18b473a45e7e")],
-		// PHYS 165
-		[ObjectId("62728f25faca18b473a45e7f")],
-		// PHYS 195
+		[ObjectId("627290194e19b2ffd466b9d1")],
+		//STAT 164
+		[ObjectId("627290194e19b2ffd466b9d2")],
+		//BIO 120
+		[ObjectId("627290194e19b2ffd466b9d3")],
+		//BIO 142
 		[ObjectId("626d42befee24c0a7a8e930e")],
 		// COMM 10
-		[ObjectId("628126ce019bd45416993ca3")],
-		// APHY 198
-		[ObjectId("62728f25faca18b473a45e80")],
-		// APHY 200
-		[ObjectId("62728f25faca18b473a45e81")],
-		// PHYS 142
-		[ObjectId("62728f25faca18b473a45e82")],
-		// APHY 191
-		[ObjectId("62728f25faca18b473a45e83")],
-		// APHY 199
-		[ObjectId("626d42befee24c0a7a8e9312")]
-		// STS 1
+		[ObjectId("627290194e19b2ffd466b9d4")],
+		// BIO 195
+		[ObjectId("627290194e19b2ffd466b9d5")],
+		//BIO 127
+		[ObjectId("626d42befee24c0a7a8e930f")],
+		// ETHICS 1
+		[ObjectId("628132c30a2229912b1b909f")],
+		//BIO 198
+		[ObjectId("62728606f5ec17dac940459d")],
+		// COMA 150
+		[ObjectId("627290194e19b2ffd466b9d6")],
+		// BIO 199
+		[ObjectId("627290194e19b2ffd466b9d7")]
+		// BIO 200
 	],
-	'specializationUnits': 21,
+	'specializationUnits': 33,
 	'geElectiveUnits': 9,
 	'maxThesisIterations': 9,
 };
