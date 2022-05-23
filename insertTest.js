deb139acb42863d430c51947eb7c800b75a95dd0
Mirella Arguelles <marguelles@up.edu.ph>
2022-05-24 01:17:11 +0800

    curriculum: new BSCS SP

--- insertTest.js
+++ insertTest.js
@@ -45,9 +45,9 @@ var ObjectId = require('mongodb').ObjectId;
 // add required subjects, and change specializationUnits to 0 since they are
 // explicitly stated in the curricula.
 
-const degree = {
+const degree2 = {
 	'name': 'BSCS',
-	'major': 'Thesis',
+	'major': 'SP',
 	'subjects': [
 		[ObjectId('62728d04ecce07f44e4d1761')],
 		// {
@@ -239,15 +239,15 @@ const degree = {
 		// 	"code" : "CMSC 198",
 		// 	"units" : 3
 		// },
-		[ObjectId('627292c8486fcb85aa703ac7')],
+		[ObjectId("627292c8486fcb85aa703ac9")],
+		// {
+		// 	"code" : "CMSC 190",
+		// 	"units" : 3
+		// },
+		[ObjectId("627292c8486fcb85aa703ac7")],
 		// {
 		// 	"code" : "CMSC 199",
 		// 	"units" : 1
-		// },
-		[ObjectId('627292c8486fcb85aa703ac8')],
-		// {
-		// 	"code" : "CMSC 200",
-		// 	"units" : 3
 		// },
 		[ObjectId('627292c8486fcb85aa703aca')],
 		// {
@@ -255,229 +255,14 @@ const degree = {
 		// 	"units" : 3
 		// },
 	],
-	'specializationUnits': 15,
-	'geElectiveUnits': 9,
-	'maxThesisIterations': 9,
-};
-
-const degree2 = {
-	'name': 'BSCS',
-	'major': 'SP',
-	'subjects': [
-		ObjectId('62728d04ecce07f44e4d1761'),
-		// {
-		// 	"code" : "CMSC 12",
-		// 	"units" : 3
-		// },
-		ObjectId('627292bbe42248877729dd81'),
-		// {
-		// 	"code" : "CMSC 56",
-		// 	"units" : 3
-		// },
-		ObjectId('626d42befee24c0a7a8e930f'),
-		// {
-		// 	"code" : "ETHICS 1",
-		// 	"units" : 3
-		// },
-		ObjectId('62728cc3591bd41f02539496'),
-		// {
-		// 	"code" : "MATH 27",
-		// 	"units" : 3
-		// },
-		ObjectId('626d42befee24c0a7a8e9312'),
-		// {
-		// 	"code" : "STS 1",
-		// 	"units" : 3
-		// },
-		ObjectId('626d44d7eb3a38230c7a3e76'),
-		// {
-		// 	"code" : "HK 11",
-		// 	"units" : 0
-		// },
-		ObjectId('626d42befee24c0a7a8e930d'),
-		// {
-		// 	"code" : "ARTS 1",
-		// 	"units" : 3
-		// },
-		ObjectId('627292bbe42248877729dd82'),
-		// {
-		// 	"code" : "CMSC 21",
-		// 	"units" : 3
-		// },
-		ObjectId('627292bbe42248877729dd83'),
-		// {
-		// 	"code" : "CMSC 57",
-		// 	"units" : 3
-		// },
-		ObjectId('62728d04ecce07f44e4d1759'),
-		// {
-		// 	"code" : "MATH 28",
-		// 	"units" : 3
-		// },
-		ObjectId('626d42befee24c0a7a8e9310'),
-		// {
-		// 	"code" : "KAS 1",
-		// 	"units" : 3
-		// },
-		ObjectId('626d42befee24c0a7a8e9311'),
-		// {
-		// 	"code" : "HIST 1",
-		// 	"units" : 3
-		// },
-		ObjectId('626d44d7eb3a38230c7a3e7a'),
-		// {
-		// 	"code" : "HK 12",
-		// 	"units" : 0
-		// },
-		ObjectId('626d44d7eb3a38230c7a3e7b'),
-		// {
-		// 	"code" : "HK 13",
-		// 	"units" : 0
-		// },
-		ObjectId('627292bbe42248877729dd84'),
-		// {
-		// 	"code" : "CMSC 22",
-		// 	"units" : 3
-		// },
-		ObjectId('627292bbe42248877729dd85'),
-		// {
-		// 	"code" : "CMSC 123",
-		// 	"units" : 3
-		// },
-		ObjectId('627292bbe42248877729dd86'),
-		// {
-		// 	"code" : "CMSC 130",
-		// 	"units" : 3
-		// },
-		ObjectId('627292bbe42248877729dd87'),
-		// {
-		// 	"code" : "CMSC 150",
-		// 	"units" : 3
-		// },
-		ObjectId('626d42befee24c0a7a8e9313'),
-		// {
-		// 	"code" : "PI 10",
-		// 	"units" : 3
-		// },
-		ObjectId('626d44d7eb3a38230c7a3e81'),
-		// {
-		// 	"code" : "NSTP 1",
-		// 	"units" : 0
-		// },
-		ObjectId('627292bbe42248877729dd88'),
-		// {
-		// 	"code" : "CMSC 23",
-		// 	"units" : 3
-		// },
-		ObjectId('627292bbe42248877729dd89'),
-		// {
-		// 	"code" : "CMSC 100",
-		// 	"units" : 3
-		// },
-		ObjectId('627292bbe42248877729dd8a'),
-		// {
-		// 	"code" : "CMSC 127",
-		// 	"units" : 3
-		// },
-		ObjectId('627292bbe42248877729dd8b'),
-		// {
-		// 	"code" : "CMSC 131",
-		// 	"units" : 3
-		// },
-		ObjectId('62728df25f73ec537aa62c24'),
-		// {
-		// 	"code" : "STAT 101",
-		// 	"units" : 3
-		// },
-		ObjectId('626d44d7eb3a38230c7a3e85'),
-		// {
-		// 	"code" : "NSTP 2",
-		// 	"units" : 0
-		// },
-		ObjectId('626d42befee24c0a7a8e930e'),
-		// {
-		// 	"code" : "COMM 10",
-		// 	"units" : 3
-		// },
-		ObjectId('627292c8486fcb85aa703abd'),
-		// {
-		// 	"code" : "CMSC 124",
-		// 	"units" : 3
-		// },
-		ObjectId('627292c8486fcb85aa703abe'),
-		// {
-		// 	"code" : "CMSC 125",
-		// 	"units" : 3
-		// },
-		ObjectId('627292c8486fcb85aa703abf'),
-		// {
-		// 	"code" : "CMSC 132",
-		// 	"units" : 3
-		// },
-		ObjectId('627292c8486fcb85aa703ac0'),
-		// {
-		// 	"code" : "CMSC 141",
-		// 	"units" : 3
-		// },
-		ObjectId('627292c8486fcb85aa703ac1'),
-		// {
-		// 	"code" : "CMSC 170",
-		// 	"units" : 3
-		// },
-		ObjectId('627292c8486fcb85aa703ac2'),
-		// {
-		// 	"code" : "CMSC 128",
-		// 	"units" : 3
-		// },
-		ObjectId('627292c8486fcb85aa703ac3'),
-		// {
-		// 	"code" : "CMSC 137",
-		// 	"units" : 3
-		// },
-		ObjectId('627292c8486fcb85aa703ac4'),
-		// {
-		// 	"code" : "CMSC 142",
-		// 	"units" : 3
-		// },
-		ObjectId('627292c8486fcb85aa703ac5'),
-		// {
-		// 	"code" : "CMSC 173",
-		// 	"units" : 3
-		// },
-		ObjectId('627292c8486fcb85aa703ac6'),
-		// {
-		// 	"code" : "CMSC 180",
-		// 	"units" : 3
-		// },
-		ObjectId('6283e8ed0175f7122e43e495'),
-		// {
-		// 	"code" : "CMSC 198",
-		// 	"units" : 3
-		// },
-		ObjectId('627292c8486fcb85aa703ac7'),
-		// {
-		// 	"code" : "CMSC 190",
-		// 	"units" : 3
-		// },
-		ObjectId('627292c8486fcb85aa703ac8'),
-		// {
-		// 	"code" : "CMSC 199",
-		// 	"units" : 1
-		// },
-		ObjectId('627292c8486fcb85aa703aca'),
-		// {
-		// 	"code" : "ENG 10",
-		// 	"units" : 3
-		// },
-	],
 	'specializationUnits': 18,
 	'geElectiveUnits': 9,
-	'maxThesisIterations': 9,
+	'maxThesisIterations': 3,
 };
 
 const go = async () => {
 	try {
-		let res = await Degree.create(degree);
+		let res = await Degree.create(degree2);
 		console.log(res);
 		console.log('Inserted degree program');
 	} catch (e) {
