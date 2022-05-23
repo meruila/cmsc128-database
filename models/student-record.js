6d4835d45a01625861b9cba9ca75ce0b2a71ad57
Mirella Arguelles <marguelles@up.edu.ph>
2022-05-24 02:23:24 +0800

    schema: added remarks for SHAC committee in student record schema

--- models/student-record.js
+++ models/student-record.js
@@ -7,62 +7,35 @@
  *          - last                  (String)
  *      Student Number              (String)
  *      Degree Program              (Reference to Degree model)
- *      Total Units Taken           (Number)
- *      GWA                         (Number)
+ *      Total Units Taken           (String)
+ *      GWA                         (String)
  *      Records                     (Array)
- *          - Semester              (Number)
+ *          - Semester              (String)
  *          - Academic Year         (String)
  *          - Subjects Taken        (Array)
- *              - Subject Code      (Reference to Subject model)
- *              - Grade             (String)
- *              - Gradepoint        (Number)
- *              - Summation         (Number)
- *          - Units                 (Number)
+ *          - Units                 (String)
  *          - Status                (String)
  *      Verified By                 (Array) (Reference to Admin or User model)
- * 
+ *      Remarks                     (String)
 ********************************/
 
 const mongoose = require("mongoose");       
 
-//#region subdocument schema
-//taken subject schema
-const TakenSubject_Schema = new mongoose.Schema({
-    subjectCode: {
-        type: mongoose.ObjectId,
-        required: true,
-        ref: 'Subject'
-    },
-    grade : {
-        type: String,
-        required: true
-    },
-    gradepoint : {
-        type: Number,
-        required: true
-    },
-    summation : {
-        type: Number,
-        required: true
-    }
-});
-
-//semester schema
+// //semester schema
 const Semester_Schema = new mongoose.Schema({
     sem: {
-        type: Number,
+        type: String,       // originally Number, made it String to accommodate midyear (M)
         required: true
     },
-    acadYear: {
+    year: {
         type: String,
         required: true
     },
     subjects: {
-        type: [TakenSubject_Schema],
-        required: true
+        type: [[String]]
     },
     units: {
-        type: Number,
+        type: String,        // originally Number
         required: true
     },
     status: {
@@ -76,26 +49,34 @@ const Semester_Schema = new mongoose.Schema({
 const StudentRecord_Schema = new mongoose.Schema({
     //#region single-valued attribute
     name: {
-        fname: {
+        last: {
             type: String,
             required: true
         },
-        lname: {
+        first: {
             type: String,
             required: true
         }
     },
-    studentNumber : {
-        type: String,                               //string ba talaga, hindi number??? bale pwede kasi na wala na yung dash
-        unique: true,                               // Reply: yes string siya. tama ka na number technically yung student number pero hindi naman 
-        required: true                              //        kasi need gawan ng arithmetic yung pagka-"number" niya, kaya mas convenient na
-    },                                              //        string na lang. Usually need ang first 4 numbers ng student number (e.g. 2019-XXXXX)
-    degreeProgram: {                                //        and mas madali/dynamic i-extract yung data na yun sa pag-split ng student number using 
-        type: mongoose.ObjectId,                    //        the dash imbis na gumamit ng indexing
+    course: {                              
+        type: String,                  
         required: true,
-        ref: 'Degree'
+        // ref: 'Degree'
+    },
+    studentNo : {
+        type: String,                               
+        unique: true,                               
+        required: true                             
+    }, 
+    records: {
+        type: [Semester_Schema],
+        required: true
     },
     totalUnitsTaken: {
+        type: Number,
+        required: true
+    },
+    totalUnitsRequired: {
         type: Number,
         required: true
     },
@@ -103,27 +84,18 @@ const StudentRecord_Schema = new mongoose.Schema({
         type: Number,
         required: true
     },
-    //#endregion
-
-    //#region multi-valued attribute
-    records: {
-        type: [Semester_Schema],
+    uploadedBy: {
+        type: String,
         required: true
     },
-    verifiedBy: [{
-        type: mongoose.ObjectId,
-        //reference to admin or user model/collection
-        refPath: 'modelReferences' 
-    }],
-
-    //to reference two possible models
-    modelReferences: {
-        type: String,
-        required: true,
-        enum: ['Admin', 'User']
+    
+    verifiedBy: {
+        type: [mongoose.ObjectId]
+    },
+    remarks: {
+        type: String
     }
-    //#endregion
 },
 { collection : 'student-record' });
 
-module.exports = mongoose.model("Student-Record", StudentRecord_Schema);
+module.exports = mongoose.model("StudentRecord", StudentRecord_Schema);
