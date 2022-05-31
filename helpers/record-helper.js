82b16dcff1dd1716094f6e8ba784751d19b21d8e
Mirella Arguelles <marguelles@up.edu.ph>
2022-05-31 16:44:07 +0800

    helpers: sign and unsign verification of student record

--- helpers/record-helper.js
+++ helpers/record-helper.js
@@ -1,21 +1,35 @@
 const mongoose = require("mongoose");
 const StudentRecord = require("../models/student-record");
 const User = require("../models/regular-user");
+const dbLog = require("./log-helper")
+var ObjectId = require('mongodb').ObjectId;
+
+const getCurrentTime = () => {
+    var today = new Date();
+    var date = (today.getMonth()+1)+'-'+today.getDate()+'-'+today.getFullYear();
+    var time = today.getHours() + ":" + today.getMinutes();
+    var dateTime = date+' '+time;
+    return dateTime;
+ }
 
 /*
 Given a student number, save a student record to database
+TODO: send reasons why student record was not saved
 */
 exports.saveRecord = async(recs, email) => {
     let savedList = [];
+    let errList = [];
     try{
         if (recs.length === 0) {
             return savedList;// empty array of records
         }
+        let nowTime = getCurrentTime();
         
         for (i in recs){
             let record = recs[i].data;
             let buffer = await StudentRecord.findOne({ "studentNo": record.studentNo });
-            if (buffer != null){ // if user is found, do not save
+            if (buffer != null){ // if student record is found, do not save
+                errList.push({filename: recs[i].filename, err: "Student record already exists."});
                 continue;
             }
             let studentRecord = {
@@ -29,19 +43,19 @@ exports.saveRecord = async(recs, email) => {
                 totalUnitsTaken: record.totalUnitsTaken,
                 totalUnitsRequired: record.totalUnitsRequired,
                 GWA: record.GWA,
-                verifiedBy: record.verifiedBy,
+                verifiedBy: [],
                 uploadedBy: email
             }
+
             let studentDocument = new StudentRecord(studentRecord);
             await studentDocument.save();
+            await dbLog.saveLog(nowTime, "Added", record.studentNo, email);
             savedList.push(recs[i].filename);
         }
-        return savedList;
+        return {savedList: savedList, errList:errList};
     }catch (e){
-        throw {e: e, savedList: savedList};
+        throw {e: e, savedList: savedList, errList:errList};
     }
-    
-    
 }
 
 /*
@@ -80,13 +94,21 @@ exports.editStudentRecord = async(record) => {
         if (buffer == null) {
             return false; //student does not exist
         }
-        const studentRecord = new StudentRecord(record);
-        await StudentRecord.replaceOne({
-            "studentNo": record.studentNo
-        }, studentRecord )
-        // await StudentRecord.replaceOne({
-        //     "studentNumber": record.studentNo
-        // }, { studentRecord } )
+        // const studentRecord = new StudentRecord(record);
+        await StudentRecord.updateOne({
+            studentNo: record.studentNo
+        }, {
+            $set: {
+                "name.last": record.name.last,
+                "name.first": record.name.first,
+                "course": record.course,
+                "records": record.records,
+                "totalUnitsTaken": record.totalUnitsTaken,
+                "totalUnitsRequired": record.totalUnitsRequired,
+                "GWA": record.GWA
+            }
+        })
+        
         return true;   
     }
     catch(e) {
@@ -113,47 +135,148 @@ exports.delStudentRecord = async(studentNo) => {
 }
 
 exports.showSummary = async() => {
-    try {
-        try{        
-            const recs = await StudentRecord.find({});
-            if (recs.length == 0) {
-                return recs;
-            }
-            let records = [];
-            for (i in recs) {
-                record = {
-                    studentNo: recs[i].studentNo,
-                    name: recs[i].name,
-                    course: recs[i].course,
-                    GWA: recs[i].GWA,
-                    verifiedBy: []
-                }
-                if (recs[i].verifiedBy.length < 2) {
-                    continue;
-                }
-                for (j in recs[i].verifiedBy) {
-                    let user = await User.findOne({ "_id": recs[i].verifiedBy[j] });
-                    record.verifiedBy.push(user.user.email);
-                }
-                records.push(record);
-            }
-            return records;
-        }catch(e){
-            console.log(e);
-            throw(e);
+    try {      
+        const recs = await StudentRecord.find({});
+        if (recs.length == 0) {
+            return recs;
         }
+        let records = [];
+        for (i in recs) {
+            record = {
+                studentNo: recs[i].studentNo,
+                name: recs[i].name,
+                course: recs[i].course,
+                GWA: recs[i].GWA,
+                verifiedBy: []
+            }
+            
+            // check if verifiedBy is less than 2
+            if (recs[i].verifiedBy.length < 2) {
+                continue;
+            }
+            for (j in recs[i].verifiedBy) {
+                record.verifiedBy.push(recs[i].verifiedBy[j]);
 
-//         > array of student records. Array contains:
-        // {
-        //   studentNo: {}
-        //   {name: last{}, first{}},
-        //   course: {},
-        //   GWA: {},
-        //   verificationStatus: {}
-        // }
+            }
+            records.push(record);
+        }
+        return records;
 
     } catch(e) {
-        throw (e);
+        console.log(e);
+        throw e;
+    }
+}
+
+exports.affixSign = async(studentNo, email) => {
+    try {
+        const viewRecord = await StudentRecord.findOne(
+            {studentNo: studentNo}
+        )
+        
+        if (viewRecord.verifiedBy.includes(email)) {
+            return false;
+        }
+
+        const record = await StudentRecord.updateOne(
+            { studentNo: studentNo },
+            { $push: { verifiedBy: email } }
+        )
+
+        if (record === null) {
+            return false;
+        }
+        
+        // record.verifiedBy.push(user_objectID);
+        return true;
+    } catch (e) {
+        throw e;
+    }
+    
+}
+
+recordsCountHelper = async() => {
+    try {
+        const allStudentRecords = StudentRecord.find();
+        const verified = StudentRecord.find({'verifiedBy.1':{$exists:true}});
+        
+        return Promise.all([allStudentRecords, verified]);
+    } catch (e) {
+        throw e;
+    }
+}
+
+exports.recordsCount = async() => {
+    try{
+        const records = await recordsCountHelper();
+        return [records[0].length, records[1].length];
+    } catch(e) {
+        throw e;
+    }
+        
+}
+
+exports.adminRemoved = async(studentNo, email) => {
+    const record = await StudentRecord.updateOne(
+        { studentNo: studentNo },
+        { $set: { verifiedBy: [] } }
+    )
+
+    if (record.modifiedCount <= 0) {
+        return false;
+    }
+    return true;
+}
+
+exports.removeSign = async(studentNo, email) => {
+    try {
+        const viewRecord = await StudentRecord.findOne(
+            { studentNo: studentNo }
+        )
+
+        const user = await User.findOne(
+            { 'user.email': email }
+        )
+
+        if (!viewRecord || !user){
+            console.log("email q: ", email);
+            console.log("EMPTY record/user", user);
+            return false;
+        }
+
+        if (viewRecord.verifiedBy.length === 0 || viewRecord.verifiedBy.length > 1) {
+            // console.log("nagkaproblema sa length ni verfied by ", viewRecord.verifiedBy.length);
+            return false;
+        }
+
+        if (user.user.email == viewRecord.verifiedBy[0]) {
+            const record = await StudentRecord.updateOne(
+                { studentNo: studentNo },
+                { $set: { verifiedBy: [] } }
+            )
+        } else {
+            console.log("hindi ako yung first :((", viewRecord.verifiedBy[0]);
+            return false;
+        }
+        
+        // record.verifiedBy.push(user_objectID);
+        return true;
+    } catch (e) {
+        throw e;
+    }
+}
+
+exports.delStudentRecords = async() => {
+    try {
+        const records = await recordsCountHelper();
+        const toDelete = await StudentRecord.deleteMany();
+        if (toDelete.deletedCount == records[0].length) {
+            return true;
+        } else {
+            return false;
+        }
+    } catch (e) {
+        throw e;
     }
 }
 
