a8033d46e824ff081139150d907a2149f0c81aa8
Mirella Arguelles <marguelles@up.edu.ph>
2022-06-06 20:37:35 +0800

    feat: initialize data for subjects and curriculum without mongodump

---
+++ helpers/init-database.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+
+// mongoose.connect(
+// 	'mongodb://localhost:27017/shac-database',
+// 	{ useNewUrlParser: true, useUnifiedTopology: true },
+// 	(err) => {
+// 		if (err) {
+// 			console.log(err);
+// 		} else {
+// 			console.log('Successfully connected to Mongo DB');
+// 		}
+// 	}
+// );
+
+/*
+	init-database initializes the data for the CAS subjects and 2018 curricula
+*/
+
+const curriculum = require('./curriculum/degree_programs');
+const subj = require('./curriculum/subjects');
+const Subject = require('../models/subject.js');
+const Degree = require('../models/curriculum');
+
+const loadSubjects = async () => {
+	try {
+		const subjectList = await Subject.find({});
+		if (subjectList.length == 0) {
+			for (let i in subj) {
+				await Subject.create(subj[i]);
+			}
+			console.log("Loaded subjects in db");
+		} else {
+			console.log("Subjects already in db");
+		}
+	} catch (e) {
+		console.log(e);
+	}
+};
+
+const loadCurricula = async() => {
+	try {
+		const curriculumList = await Degree.find({});
+		if (curriculumList.length == 0) {
+			for (let i in curriculum) {
+				await Degree.create(curriculum[i]);
+			}
+			console.log("Loaded curricula in db");
+		} else {
+			console.log("All curriculum already in db");
+		}
+	} catch (e) {
+		console.log(e);
+	}
+}
+
+exports.loadData = async() => {
+	await loadSubjects();
+	await loadCurricula();
+}
